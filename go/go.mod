f62663c6584f23465692a0da96b1a633ae4f7375
yihsuan <cyhkelvin0530@gmail.com>
2022-03-01 17:39:23 +0800

    update server/ and go.mod, go.sum

--- go/go.mod
+++ go/go.mod
@@ -3,3 +3,5 @@ module github.com/cyhkelvin/voskgo_rule/go
 go 1.16
 
 replace github.com/cyhkelvin/voskgo_rule/go => ./
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ go/server/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	//"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	//"time"
+	//"os"
+	//"strconv"
+	"unsafe"
+
+	vosk "github.com/cyhkelvin/voskgo_rule/go"
+)
+
+type Message struct {
+	Result []struct {
+		Conf  float64
+		End   float64
+		Start float64
+		Word  string
+	}
+	Text string
+}
+
+var m Message
+
+func gen_wb_msg(res []byte) string {
+	err := json.Unmarshal(res, &m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//t := time.Now().Format("2006-01-02 15:04:05")
+	//msg := fmt.Sprintf("final %s %s", string(m.Text[:]), t)
+	//return msg
+	return string(m.Text[:])
+}
+
+func check(err error, msg string) bool {
+	if err != nil {
+		log.Println(msg)
+		log.Println(err)
+		return true
+	}
+	return false
+}
+
+func main() {
+	// initial model and recognizer
+	model, err := vosk.NewModel("model")
+	_ = check(err, "[asr-server] error: model loading error!")
+
+	sampleRate := 8000.0 //16000.0
+	rec, err := vosk.NewRecognizer(model, sampleRate)
+	_ = check(err, "[asr-server] error: reconizer init error!")
+	rec.SetWords(1)
+
+	upgrader := &websocket.Upgrader{
+		//如果有 cross domain 的需求，可加入這個，不檢查 cross domain
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		c, err := upgrader.Upgrade(w, r, nil)
+		if check(err, "[asr-server] upgrade error") {
+			return
+		}
+		defer func() {
+			log.Println("disconnect !!")
+			c.Close()
+		}()
+		params := r.URL.Query()
+		session_id := params.Get("id")
+		log.Printf("receive: %s\n", session_id)
+		for {
+			mtype, msg, err := c.ReadMessage()
+			if check(err, "[asr-server] read error!") {
+				break
+			}
+
+			if rec.AcceptWaveform(msg) != 0 {
+				wb_msg := gen_wb_msg(rec.Result())
+				log.Printf("reconize: %s\n", wb_msg)
+				err = c.WriteMessage(mtype, []byte(wb_msg))
+				_ = check(err, "[asr-server] write error.")
+				break
+			}
+			log.Printf("receive size: %d\n", unsafe.Sizeof(msg))
+			//log.Printf("receive: %s\n", msg)
+			//err = c.WriteMessage(mtype, msg)
+			//if check(err, "[asr-server] write error!"){
+			//	break
+			//}
+
+			if bytes.Equal(msg, []byte("{\"eof\" : 1}")) {
+				break
+			}
+		}
+		res_msg := gen_wb_msg(rec.FinalResult())
+		err = c.WriteMessage(1, []byte(res_msg))
+		_ = check(err, "[asr-server] write error!")
+		log.Println("result:", res_msg)
+
+		err = c.WriteMessage(1, []byte("{\"msg\": \"ENDDD\"}"))
+		_ = check(err, "[asr-server] end error")
+	})
+	log.Println("server start at :8900")
+	log.Fatal(http.ListenAndServe(":8900", nil))
+}
